861942bf38697e2b7f3326a077872525dc027455
Jemel Williams <jwilliams25400@yahoo.com>
2021-08-29 16:01:46 -0400

    readme now generating  need to fix display

--- index.js
+++ index.js
@@ -1,98 +1,97 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const path = require("path")
-const inquirer = require("inquirer")
-const generateMarkdown = require("./utils/generateMarkdown")
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = () => 
-// inquirer.prompt
-   ([
-    {
-        type: "input",
-        name: "author",
-        message: "What is your author's name?",
-        validate: (value) =>{ if(value){return true} else {return "please enter a author's to continue"}}        
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username?",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "please enter a github username to continue";
+      }
     },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your github name?",
-        validate: (value) =>{ if(value){return true} else {return "please enter a github username to continue"}}
+  },
+  {
+    type: "input",
+    name: "E-mail",
+    message: "What is your E-mail?",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "please enter a E-mail to continue";
+      }
     },
-    {
-        type: "input",
-        name: "E-mail",
-        message: "What is your E-mail?",
-        validate: (value) =>{ if(value){return true} else {return "please enter a E-mail to continue"}}
+  },
+  {
+    type: "input",
+    name: "ProjectTitle",
+    message: "What is your Project title?",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "please enter a Project title to continue";
+      }
     },
-    {
-        type: "input",
-        name: "Project title",
-        message: "What is your Project title?",
-        validate: (value) =>{ if(value){return true} else {return "please enter a Project title to continue"}}
-    },
-    {
-        type: "input",
-        name: "Description",
-        message: "Please write a brief a description of your project",
-    },
-    {
-        type: "list",
-        name: "License",
-        message: "What kind of license does your project have?",
-        choices: ["MIT", "APACE 2.2", "BSD", "GPL 3.0", "unlicensed"]
-    },
-    {
-        type: "input",
-        name: "User Issue solved",
-        message: "what problem does your application solve?"
-    },
-    {
-        type: "input",
-        name: "discoveries",
-        message: "what did you learn from this Assignment?"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "what command should be run to install dependencies?"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "what should the user knwo to run your program?"
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "Please write a brief a description of your project",
+  },
+  {
+    type: "checkbox",
+    name: "technology",
+    message: "what languages are used on your project",
+    choices: ["HTML", "CSS", "Javascript", "Node"],
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license does your project have?",
+    choices: ["MIT", "APACE 2.2", "BSD", "GPL 3.0", "unlicensed"],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "what command should be run to install dependencies?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "what should the user know to run your program?",
+  },
+  {
+    type: "input",
+    name: "Contribute",
+    message:
+      "How can others developers contribute to the repo?",
+  },
+];
 
-    },
-    {
-        type: "input",
-        name: "collaborators",
-        message: "are there any individual(s) whom you collaborated with you build the App?"
-    }
-
-]);
+//   .then((response) => {
+//       console.log (response)
+//   });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data,) {
-    fs.writeFile(fileName, data, function(err){
-        console.log(fileName)
-        console.log(data)
-        if (err) {
-            return console.log(err)
-        }
-        else{
-            console.log("Successfully wrote: " + fileName);
-        }
-    })
+function writeToFile(fileName, response) {
+  return fs.writeFile(fileName, JSON.stringify(response), (err) =>
+    err ? console.error(err) : console.log("Successfully wrote: " + fileName)
+  );
 }
-
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt(questions)
-.then(function(data){
-    writeToFile("README.md", generateMarkdown(data));
-    console.log(data)
-})
+  inquirer.prompt(questions).then((response) => {
+    writeToFile("README.md", generateMarkdown(response));
+  });
 }
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,90 +1,107 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let licenseBadge = "";
-  if(license === "MIT") {
-    licenseBadge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  if (license !== "Unlicensed") {
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`;
+  } else {
+    return "";
   }
-  else if (license === "Apache License 2.0") {
-    licenseBadge = `![License: Apache License](https://img.shields.io/badge/License-Apache-green.svg)`  
-  }
-  else if (license === "Mozilla Public License") {
-    licenseBadge = `![License: Mozilla Public License](https://img.shields.io/badge/License-Mozilla-red.svg)`
 }
-  else if (license === "GPL v3") {
-    licenseBadge = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
-}
-  else if (license === "unlicensed") {
-    licenseBadge = `![License: unlicense](https://img.shields.io/badge/License-unlicense-blue.svg)`
 
-}
-}
+//   if(license === "MIT") {
+//     licenseBadge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+//   }
+//   else if (license === "Apache License 2.0") {
+//     return `![License: Apache License](https://img.shields.io/badge/License-Apache-green.svg)`
+//   }
+//   else if (license === "Mozilla Public License") {
+//     return `![License: Mozilla Public License](https://img.shields.io/badge/License-Mozilla-red.svg)`
+// }
+//   else if (license === "GPL v3") {
+//     return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+// }
+//   else if (license === "unlicensed") {
+//     return `![License: unlicense](https://img.shields.io/badge/License-unlicense-blue.svg)`
+
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  let licenseLink = "";
   if (license === "MIT") {
-    licenselink = `[License: MIT](https://opensource.org/licenses/MIT)`
-}
-  else if (license === "Apache License 2.0") {
-    licenselink = `[License: Apache License](https://apache.org/licenses/License-2.0)`
-}
-  else if (license === "Mozilla Public License") {
-    licenselink = `[License: Mozilla Public License](https://opensource.org/licenses/MPL-2.0)`
-}
-  else if (license === "GPL v3") {
-    licenselink = `[License: Mozilla Public License](https://gnp.org/licenses)`
+    licenselink = `[License: MIT](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Apache License 2.0") {
+    licenselink = `[License: Apache License](https://apache.org/licenses/License-2.0)`;
+  } else if (license === "Mozilla Public License") {
+    licenselink = `[License: Mozilla Public License](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "GPL v3") {
+    licenselink = `[License: Mozilla Public License](https://gnp.org/licenses)`;
+  } else {
+    return "";
   }
-  else {
-    return("");
-  }
-
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (License === "unlicensed") {
+  if (license === "unlicensed") {
     return "";
-  }
-  else {
+  } else {
     return "* [License](#license)";
   }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  https:\\www.github.com/${data.username}/${data.title}
+const badge = renderLicenseBadge(data.license);
+const licenselink = renderLicenseLink(data.license);
+const licenseSec = renderLicenseSection(data.license)
+
+  return `# ${data.ProjectTitle}
+  ### https:\\www.github.com/${data.github}
+
   This Project is licensed under:
-  ${renderLicenseBadge(data.license)}
-  ${badge}
+  ${badge}${licenselink}
+
+  ## Technology
+  ${data.technology}
+
   ## Description
-  ${data.description}
+  ${data.Description}
+
   ## Table of Contents:
-  ${renderLicenseLink(data.license)}
+
   * [Installation](#installation)
+
   * [Usage](#usage)
+
   * [Contributing](#contributing)
+
   * [Test](#test)
+
   * [Questions](#questions)
+
   ### Installations:
   To ensure the program run open the terminal and run the following:
   \`\`\`${data.installation}\`\`\`
+
   ## Usage:
   ${data.usage}
+  
   ## Test:
-  In order to test the Project open the terminal and run the following:
-  ${data.test}
+  In order to run the Project open the terminal and run the following:
+  ${data.testCommand}
+
   ## Contributing:
-  ${data.contribute}
+  ${data.Contribute}
+
   ## License:
+  ${licenseSec}
+
   This Project is licensed under:
   ${data.license}
-  \`\`\`${data.tests}\`\`\`
+  
   ### Questions:
-  If you have any questions about the repo, contact [Github](https://www.github.com/${data.username})
-   or contact ${data.author} at ${data.email}
+  If you have any questions about the repo, contact Github: (https://www.github.com/${data.github})
+  or E-mail: ${data.Email}
 `;
 }
 
